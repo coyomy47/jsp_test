1dc2fd229dabda164c6b47fe0dd21bd2b0670884
Han, Sangyoen <coyomy@naver.com>
2020-05-06 12:40:12 +0900

    20분

---
+++ src/jsp_test/Book.java
@@ -0,0 +1,47 @@
+package jsp_test;
+
+public class Book {
+
+	int id;
+	String title;
+	String author;
+    String categoryName;
+    int price;
+    String publisher;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+}
